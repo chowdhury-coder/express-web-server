4c6aa1b675d0a648074e8e7fe1d70f2499374be8
Chowdhury <mohammad.chowdhury@clover.com>
2022-12-19 13:25:09 +0100

    Updatedand added README.md

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express")
-const path = require("path")
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static("public"))
 
 app.get('/now', (request, response)=>{
     const date = new Date()
